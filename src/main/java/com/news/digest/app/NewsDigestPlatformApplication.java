bedbbcb670bb2902de2689d42825f6dbf86964e2
Mayank Yadav <mayankyadav6655@gmail.com>
2025-12-11 23:23:03 +0530

    First Commit

---
+++ src/main/java/com/news/digest/app/NewsDigestPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.news.digest.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsDigestPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsDigestPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/news/digest/app/NewsDigestPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.news.digest.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsDigestPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
